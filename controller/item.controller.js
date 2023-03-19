3d91cb3820941250baa63f182df299cd1c3fc161
Alfonsfer <alfonsus.christofer@gmail.com>
2023-03-20 01:37:30 +0700

    fix getting user_id

--- controller/item.controller.js
+++ controller/item.controller.js
@@ -1,10 +1,10 @@
-const jwt = require('jsonwebtoken');
-const { Op } = require('sequelize');
-const { Item, User } = require('../database/models');
-const ErrorResponse = require('../helpers/error.helper');
-const ResponseFormat = require('../helpers/response.helper');
-const validate = require('../middleware/validation');
-const createItemSchema = require('../validation/schemas/createItem.schema');
+const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
+const { Item, User } = require("../database/models");
+const ErrorResponse = require("../helpers/error.helper");
+const ResponseFormat = require("../helpers/response.helper");
+const validate = require("../middleware/validation");
+const createItemSchema = require("../validation/schemas/createItem.schema");
 
 class ItemController {
   async getItems(req, res, next) {
@@ -19,20 +19,20 @@ class ItemController {
       // example ?numericFilters=price>1000,stock<20
       if (numericFilters) {
         const operatorMap = {
-          '>': Op.gt,
-          '>=': Op.gte,
-          '=': Op.eq,
-          '<': Op.lt,
-          '<=': Op.lte,
+          ">": Op.gt,
+          ">=": Op.gte,
+          "=": Op.eq,
+          "<": Op.lt,
+          "<=": Op.lte,
         };
         const regEx = /\b(<|>|>=|=|<|<=)\b/g;
-        numericFilters.split(',').forEach((item) => {
+        numericFilters.split(",").forEach((item) => {
           // qty>1
           const filter = item.replace(
             regEx,
             (match) => `_${match}_`,
           );
-          const filterSplit = filter.split('_');
+          const filterSplit = filter.split("_");
           queryObject[filterSplit[0]] = {
             [operatorMap[filterSplit[1]]]: filterSplit[2],
           };
@@ -43,15 +43,15 @@ class ItemController {
       let sortList = [];
       if (sort) {
         const map = {
-          A: 'ASC',
-          D: 'DESC',
+          A: "ASC",
+          D: "DESC",
         };
-        const filter = sort.split(',').forEach((data) => {
-          const [field, option] = data.split('-');
+        const filter = sort.split(",").forEach((data) => {
+          const [field, option] = data.split("-");
           sortList.push([field, map[option]]);
         });
       } else {
-        sortList.push(['created_at', 'ASC']);
+        sortList.push(["created_at", "ASC"]);
       }
 
       const page = Number(req.query.page) || 1;
@@ -65,15 +65,15 @@ class ItemController {
         order: sortList,
         limit: limit,
         offset: skip,
-        attributes: ['id', 'name', 'price', 'stock'],
+        attributes: ["id", "name", "price", "stock"],
         include: {
           model: User,
-          attributes: ['id', 'username', 'email'],
+          attributes: ["id", "username", "email"],
         },
       });
 
       if (items.length === 0) {
-        throw new ErrorResponse(404, 'No Items Found');
+        throw new ErrorResponse(404, "No Items Found");
       }
 
       return new ResponseFormat(res, 200, items);
@@ -85,9 +85,9 @@ class ItemController {
   async createItem(req, res, next) {
     try {
       const {
-        user: { user_id },
         body: { name, price, stock },
       } = req;
+      const user_id = res.locals.userId;
 
       await validate(createItemSchema, req.body);
 
@@ -115,7 +115,7 @@ class ItemController {
         include: [
           {
             model: User,
-            attributes: ['id', 'username', 'email'],
+            attributes: ["id", "username", "email"],
           },
         ],
       });
@@ -144,7 +144,7 @@ class ItemController {
       });
 
       if (!item) {
-        throw new ErrorResponse(404, 'Item Not Found');
+        throw new ErrorResponse(404, "Item Not Found");
       }
 
       await Item.update(
@@ -185,10 +185,10 @@ class ItemController {
       });
 
       if (!item) {
-        throw new ErrorResponse(404, 'Item Not Found');
+        throw new ErrorResponse(404, "Item Not Found");
       }
 
-      return new ResponseFormat(res, 200, 'Item deleted');
+      return new ResponseFormat(res, 200, "Item deleted");
     } catch (error) {
       next(error);
     }
