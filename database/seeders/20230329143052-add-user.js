77f1abfa849dbcbc70a8bd162eac0de53e7ec80f
Alfonsfer <alfonsus.christofer@gmail.com>
2023-04-03 14:18:14 +0700

    update seeder

--- database/seeders/20230329143052-add-user.js
+++ database/seeders/20230329143052-add-user.js
@@ -12,8 +12,7 @@ module.exports = {
           id: 1,
           email: "admin1@gmail.com",
           username: "admin1",
-          password:
-            "$2a$10$YMqBbUN7Nhmks.4HaWuxlOAPjDK01ipplF1YRFG7H2GqFPT1FPDN.",
+          password: await Hash.hashing("12345678"),
           role: "admin",
           created_at: new Date(),
           updated_at: new Date(),
@@ -22,8 +21,7 @@ module.exports = {
           id: 2,
           email: "user1@gmail.com",
           username: "user1",
-          password:
-            "$2a$10$YMqBbUN7Nhmks.4HaWuxlOAPjDK01ipplF1YRFG7H2GqFPT1FPDN.",
+          password: await Hash.hashing("12345678"),
           role: "user",
           created_at: new Date(),
           updated_at: new Date(),
@@ -32,8 +30,7 @@ module.exports = {
           id: 3,
           email: "user2@gmail.com",
           username: "user2",
-          password:
-            "$2a$10$YMqBbUN7Nhmks.4HaWuxlOAPjDK01ipplF1YRFG7H2GqFPT1FPDN.",
+          password: await Hash.hashing("12345678"),
           role: "user",
           created_at: new Date(),
           updated_at: new Date(),
@@ -42,8 +39,7 @@ module.exports = {
           id: 4,
           email: "user3@gmail.com",
           username: "user3",
-          password:
-            "$2a$10$YMqBbUN7Nhmks.4HaWuxlOAPjDK01ipplF1YRFG7H2GqFPT1FPDN.",
+          password: await Hash.hashing("12345678"),
           role: "user",
           created_at: new Date(),
           updated_at: new Date(),
