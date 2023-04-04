2518f1f546793ad2bcd67a0f8c8d2553e1feecb7
Alfonsfer <alfonsus.christofer@gmail.com>
2023-04-04 22:46:14 +0700

    edit order migration => only 1

--- database/migrations/20230126145533-order.js
+++ database/migrations/20230126145533-order.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.createTable("orders", {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
       },
       user_id: {
         type: Sequelize.INTEGER,
@@ -15,33 +15,36 @@ module.exports = {
         references: {
           model: "users",
           key: "id",
-          as: "user_id"
-        }
+          as: "user_id",
+        },
       },
       total: {
         type: Sequelize.DOUBLE,
-        allowNull: false
+        allowNull: false,
       },
       status: {
-        type: Sequelize.ENUM("Pending","Ordered","Complete"),
-        defaultValue: "Pending"
+        type: Sequelize.ENUM(
+          "Pending",
+          "Cancelled",
+          "Complete",
+        ),
+        defaultValue: "Pending",
       },
       created_at: {
         type: Sequelize.DATE,
-        defaultValue: Date.now()
+        defaultValue: Date.now(),
       },
       updated_at: {
         type: Sequelize.DATE,
-        defaultValue: Date.now()
+        defaultValue: Date.now(),
       },
       deleted_at: {
-        type: Sequelize.DATE
-      }
-    })
+        type: Sequelize.DATE,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('orders');
-    
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("orders");
+  },
 };
