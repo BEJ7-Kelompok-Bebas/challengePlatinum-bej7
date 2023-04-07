79c6114e8e66ad1a79a2d7d9c6f45d1b2bff17d9
Alfonsfer <alfonsus.christofer@gmail.com>
2023-04-07 23:31:23 +0700

    edit all testing

--- __test__/Integration-test/Order-integration-test/createOrder.integration.test.js
+++ __test__/Integration-test/Order-integration-test/createOrder.integration.test.js
@@ -3,75 +3,183 @@ require("dotenv").config();
 const request = require("supertest");
 const app = require("../../../app");
 
-const { sequelize } = require("../../../database/models");
+const {
+  sequelize,
+  User,
+  Item,
+  OrderItem,
+  Order,
+} = require("../../../database/models");
 const { ModuleJwt } = require("../../../modules");
 
-beforeAll(async () => {
-  await sequelize.authenticate();
-});
-
-afterAll(async () => {
-  await sequelize.close();
-});
-
-describe("Integration Testing: Order Controller", () => {
-  describe("Integration Testing: createOrder", () => {
-    it("should create new order", async () => {
-      const token = await ModuleJwt.signToken(2);
-      const resp = await request(app)
-        .post("/api/v1/order")
-        .set("content-Type", "application/json")
-        .set("Accept", "application/json")
-        .set("Authorization", `Bearer ${token}`)
-        .send({
-          data: [
-            {
-              item_id: 2,
-              qty: 5,
-              price: 8000,
-            },
-            {
-              item_id: 5,
-              qty: 3,
-              price: 5000,
-            },
-          ],
-        });
-
-      expect(resp.body).toHaveProperty("code");
-      expect(resp.body).toHaveProperty("data");
-      expect(resp.body.code).toBe(201);
-      expect(resp.body.data).toBeTruthy();
-    });
+try {
+  beforeAll(async () => {
+    await sequelize.authenticate();
+    await User.update(
+      { role: "user" },
+      { where: { id: 2 } },
+    );
+    await User.update(
+      { role: "admin" },
+      { where: { id: 1 } },
+    );
+    await Item.destroy({ where: { id: 10 } });
   });
 
-  it("should return error Item can't be ordered", async () => {
-    const token = await ModuleJwt.signToken(2);
-    const resp = await request(app)
-      .post("/api/v1/order")
-      .set("content-Type", "application/json")
-      .set("Accept", "application/json")
-      .set("Authorization", `Bearer ${token}`)
-      .send({
-        data: [
-          {
-            item_id: 10,
-            qty: 5,
-            price: 8000,
-          },
-          {
-            item_id: 5,
-            qty: 3,
-            price: 5000,
-          },
-        ],
+  afterAll(async () => {
+    await OrderItem.destroy({ where: { price: 10 } });
+    await Order.destroy({ where: { total: 80 } });
+    await Item.update({ qty: 10000 }, { where: { id: 2 } });
+    await Item.update({ qty: 10000 }, { where: { id: 5 } });
+    await sequelize.close();
+  });
+
+  describe("Integration Testing: Order Controller", () => {
+    describe("Integration Testing: createOrder", () => {
+      it("should create new order", async () => {
+        const token = await ModuleJwt.signToken(2);
+        const resp = await request(app)
+          .post("/api/v1/order")
+          .set("content-Type", "application/json")
+          .set("Accept", "application/json")
+          .set("Authorization", `Bearer ${token}`)
+          .send({
+            data: [
+              {
+                item_id: 2,
+                qty: 5,
+                price: 10,
+              },
+              {
+                item_id: 5,
+                qty: 3,
+                price: 10,
+              },
+            ],
+          });
+
+        expect(resp.body).toHaveProperty("code");
+        expect(resp.body).toHaveProperty("data");
+        expect(resp.body.code).toBe(201);
+        expect(resp.body.data).toBeTruthy();
+      });
+      it("should return error Item can't be ordered, given no item with requested id", async () => {
+        const token = await ModuleJwt.signToken(2);
+        const resp = await request(app)
+          .post("/api/v1/order")
+          .set("content-Type", "application/json")
+          .set("Accept", "application/json")
+          .set("Authorization", `Bearer ${token}`)
+          .send({
+            data: [
+              {
+                item_id: 10,
+                qty: 5,
+                price: 10,
+              },
+              {
+                item_id: 5,
+                qty: 3,
+                price: 10,
+              },
+            ],
+          });
+
+        expect(resp.body).toHaveProperty("code");
+        expect(resp.body).toHaveProperty("error");
+        expect(resp.body.code).toBe(404);
+        expect(resp.body.error).toEqual(
+          "Item can't be ordered",
+        );
       });
 
-    expect(resp.body).toHaveProperty("code");
-    expect(resp.body).toHaveProperty("error");
-    expect(resp.body.code).toBe(404);
-    expect(resp.body.error).toEqual(
-      "Item can't be ordered",
-    );
+      it("should return error Forbidden, given role is admin", async () => {
+        const token = await ModuleJwt.signToken(1);
+        const resp = await request(app)
+          .post("/api/v1/order")
+          .set("content-Type", "application/json")
+          .set("Accept", "application/json")
+          .set("Authorization", `Bearer ${token}`)
+          .send({
+            data: [
+              {
+                item_id: 10,
+                qty: 5,
+                price: 10,
+              },
+              {
+                item_id: 5,
+                qty: 3,
+                price: 10,
+              },
+            ],
+          });
+
+        expect(resp.body).toHaveProperty("code");
+        expect(resp.body).toHaveProperty("error");
+        expect(resp.body.code).toBe(403);
+        expect(resp.body.error).toEqual("Forbidden");
+      });
+
+      it("should return error Unauthorized, given no token", async () => {
+        const resp = await request(app)
+          .post("/api/v1/order")
+          .set("content-Type", "application/json")
+          .set("Accept", "application/json")
+          .send({
+            data: [
+              {
+                item_id: 10,
+                qty: 5,
+                price: 10,
+              },
+              {
+                item_id: 5,
+                qty: 3,
+                price: 10,
+              },
+            ],
+          });
+
+        expect(resp.body).toHaveProperty("code");
+        expect(resp.body).toHaveProperty("error");
+        expect(resp.body.code).toBe(401);
+        expect(resp.body.error).toEqual("Unauthorized");
+      });
+
+      it("should return error Unauthorized, given random token with different secretkey", async () => {
+        const token = await jwt.sign(
+          { user_id: 1 },
+          "diffkey",
+          { expiresIn: "1d" },
+        );
+        const resp = await request(app)
+          .post("/api/v1/order")
+          .set("content-Type", "application/json")
+          .set("Accept", "application/json")
+          .set("Authorization", `Bearer ${token}`)
+          .send({
+            data: [
+              {
+                item_id: 10,
+                qty: 5,
+                price: 10,
+              },
+              {
+                item_id: 5,
+                qty: 3,
+                price: 10,
+              },
+            ],
+          });
+
+        expect(resp.body).toHaveProperty("code");
+        expect(resp.body).toHaveProperty("error");
+        expect(resp.body.code).toBe(401);
+        expect(resp.body.error).toEqual("Unauthorized");
+      });
+    });
   });
-});
+} catch (error) {
+  console.log(error);
+}
--- __test__/Integration-test/User-integration-test/logout.integration.test.js
+++ __test__/Integration-test/User-integration-test/logout.integration.test.js
@@ -9,87 +9,90 @@ const {
 } = require("../../../database/models");
 const { ModuleJwt } = require("../../../modules");
 
-let refresh_token;
-let token;
+try {
+  let refresh_token;
+  let token;
 
-beforeAll(async () => {
-  await sequelize.authenticate();
-  refresh_token = await ModuleJwt.signRefreshToken(1);
+  beforeAll(async () => {
+    await sequelize.authenticate();
+    refresh_token = await ModuleJwt.signRefreshToken(1);
+    token = await ModuleJwt.signToken(1);
+  });
 
-  token = await ModuleJwt.signToken(1);
-});
+  afterAll(async () => {
+    await sequelize.close();
+  });
 
-afterAll(async () => {
-  await sequelize.close();
-});
+  describe("Integration Testing: User Controller", () => {
+    describe("Integration Testing: logout", () => {
+      it("should return User logged out, if everything correct", async () => {
+        const user = await User.findOne({
+          where: { id: 1 },
+          attributes: ["id", "refresh_token"],
+        });
+        user.refresh_token = refresh_token;
+        await user.save();
 
-describe("Integration Testing: User Controller", () => {
-  describe("Integration Testing: logout", () => {
-    it("should return User logged out, if everything correct", async () => {
-      const user = await User.findOne({
-        where: { id: 1 },
-        attributes: ["id", "refresh_token"],
+        const resp = await request(app)
+          .get("/api/v1/user/logout")
+          .set("content-Type", "application/json")
+          .set("Accept", "application/json")
+          .set("Cookie", `jwt=${refresh_token}`)
+          .set("Authorization", `Bearer ${token}`);
+
+        expect(resp.body).toHaveProperty("code");
+        expect(resp.body).toHaveProperty("data");
+        expect(resp.body.data).toHaveProperty("message");
+        expect(resp.body.code).toBe(200);
+        expect(resp.body.data.message).toEqual(
+          "User logged out",
+        );
       });
-      user.refresh_token = refresh_token;
-      await user.save();
 
-      const resp = await request(app)
-        .get("/api/v1/user/logout")
-        .set("content-Type", "application/json")
-        .set("Accept", "application/json")
-        .set("Cookie", `jwt=${refresh_token}`)
-        .set("Authorization", `Bearer ${token}`);
+      it("should return Error User Not Found", async () => {
+        const user = await User.findOne({
+          where: { id: 1 },
+          attributes: ["id", "refresh_token"],
+        });
+        user.refresh_token = null;
+        await user.save();
 
-      expect(resp.body).toHaveProperty("code");
-      expect(resp.body).toHaveProperty("data");
-      expect(resp.body.data).toHaveProperty("message");
-      expect(resp.body.code).toBe(200);
-      expect(resp.body.data.message).toEqual(
-        "User logged out",
-      );
-    });
+        const resp = await request(app)
+          .get("/api/v1/user/logout")
+          .set("content-Type", "application/json")
+          .set("Accept", "application/json")
+          .set("Cookie", `jwt=${refresh_token}`)
+          .set("Authorization", `Bearer ${token}`);
 
-    it("should return Error User Not Found", async () => {
-      const user = await User.findOne({
-        where: { id: 1 },
-        attributes: ["id", "refresh_token"],
+        expect(resp.body).toHaveProperty("code");
+        expect(resp.body).toHaveProperty("error");
+        expect(resp.body.code).toBe(404);
+        expect(resp.body.error).toEqual("User Not Found");
       });
-      user.refresh_token = null;
-      await user.save();
 
-      const resp = await request(app)
-        .get("/api/v1/user/logout")
-        .set("content-Type", "application/json")
-        .set("Accept", "application/json")
-        .set("Cookie", `jwt=${refresh_token}`)
-        .set("Authorization", `Bearer ${token}`);
+      it("should return User logged out, if no cookie", async () => {
+        const user = await User.findOne({
+          where: { id: 1 },
+          attributes: ["id", "refresh_token"],
+        });
+        user.refresh_token = refresh_token;
+        await user.save();
+        const resp = await request(app)
+          .get("/api/v1/user/logout")
+          .set("content-Type", "application/json")
+          .set("Accept", "application/json")
+          .set("Authorization", `Bearer ${token}`);
 
-      expect(resp.body).toHaveProperty("code");
-      expect(resp.body).toHaveProperty("error");
-      expect(resp.body.code).toBe(404);
-      expect(resp.body.error).toEqual("User Not Found");
-    });
-
-    it("should return User logged out, if no cookie", async () => {
-      const user = await User.findOne({
-        where: { id: 1 },
-        attributes: ["id", "refresh_token"],
+        expect(resp.body).toHaveProperty("code");
+        expect(resp.body).toHaveProperty("data");
+        expect(resp.body.data).toHaveProperty("message");
+        expect(resp.body.code).toBe(200);
+        expect(resp.body.data.message).toEqual(
+          "User logged out",
+        );
       });
-      user.refresh_token = refresh_token;
-      await user.save();
-      const resp = await request(app)
-        .get("/api/v1/user/logout")
-        .set("content-Type", "application/json")
-        .set("Accept", "application/json")
-        .set("Authorization", `Bearer ${token}`);
-
-      expect(resp.body).toHaveProperty("code");
-      expect(resp.body).toHaveProperty("data");
-      expect(resp.body.data).toHaveProperty("message");
-      expect(resp.body.code).toBe(200);
-      expect(resp.body.data.message).toEqual(
-        "User logged out",
-      );
     });
   });
-});
+} catch (error) {
+  console.log(error);
+}
