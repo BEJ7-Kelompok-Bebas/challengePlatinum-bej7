0a847989eaa5d46bade852102b2f28d71b3bb334
Alfonsfer <alfonsus.christofer@gmail.com>
2023-04-09 21:46:48 +0700

    Revert "add cors"
    This reverts commit 876f99d2b4acd43057d1deb53dce1fd1a8c2da8f.

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/user.routes");
 const itemRouter = require("./routes/item.routes");
 const orderRouter = require("./routes/order.routes");
-const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/item", itemRouter);
